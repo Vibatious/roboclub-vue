9e19d3e2abd34e58c79e18de1c022da77edc770f
Areeb Jamal <jamal.areeb@gmail.com>
2018-09-02 23:59:12 +0530

    WIP: Robocon

--- src/components/Other/Robocon.vue
+++ src/components/Other/Robocon.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <p>The Robocon Page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap class="mb-3">
+      <v-flex xs12 lg10 xl8 offset-lg1 offset-xl2>
+        <v-card class="green white--text">
+          <div class="text-xs-center headline pt-4 pb-4">{{robocon.title || 'Robocon'}}</div>
+          <v-progress-linear :indeterminate="loading" v-show="loading" color="green" background-color="green lighten-3"></v-progress-linear>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 lg4>
+        {{robocon.introduction}}
+      </v-flex>
+      <v-flex xs12 lg4>
+        {{ robocon.about }}
+      </v-flex>
+      <v-flex xs12 lg4>
+        {{ robocon.downloads }}
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
